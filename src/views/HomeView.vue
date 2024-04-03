e1b6934cc35fa5c5b3479649d2c56e28e5e57f2a
Austin Dreosch <austindreosch@gmail.com>
2024-04-02 23:16:08 -0700

    installed Tailwind

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,23 +1,31 @@
 <template>
   <div class="home">
-    <h3>{{ counterTitle }}</h3>
+    <h3>{{ counterData.title }}</h3>
     <div>
-      <button @click="decreaseCounter" class="btn">-</button>
-      <span class="counter">{{counter}}</span>
-      <button @click="increaseCounter" class="btn">+</button>
+      <button @click="decreaseCounter" class="btn px-5 bg-gray-100 rounded">-</button>
+      <span class="counter">{{counterData.count}}</span>
+      <button @click="increaseCounter" class="btn px-5 bg-gray-100">+</button>
     </div>
+    <div class="edit mt-5">
+      <h4 class="m-1">Edit Counter Title:</h4>
+      <input v-model="counterData.title" class="bg-gray-200 border border-black text-darkgray rounded p-1" type="text" >
+    </div>
+
   </div>
 </template>
 
 <script setup>
-  import { ref } from 'vue'
-  const counter = ref(0);
-  const counterTitle = ref('Super Counter');
+  import { reactive, ref } from 'vue'
+  const counterData = reactive({
+    count: 0,
+    title: 'Super Counter'
+  });
+
   const increaseCounter = () => {
-    counter.value++;
+    counterData.count++;
   }
   const decreaseCounter = () => {
-    counter.value--;
+    counterData.count--;
   }
 </script>
 
