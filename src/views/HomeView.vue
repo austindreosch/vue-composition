8a129c9e0062cd2a75d17191738b8884154a141b
Austin Dreosch <austindreosch@gmail.com>
2024-04-04 22:32:42 -0700

    adding programmatic examples to counter

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,11 +1,16 @@
 <template>
   <div class="home">
+    <h2 class="text-x uppercase mb-2">{{ appTitle }}</h2>
     <h3>{{ counterData.title }}</h3>
     <div>
-      <button @click="decreaseCounter" class="btn px-5 bg-gray-100 rounded">-</button>
+      <button @click="decreaseCounter(2)" class="btn px-5 bg-gray-100 rounded">-2</button>
+      <button @click="decreaseCounter(1)" class="btn px-5 bg-gray-100 rounded">-1</button>
       <span class="counter">{{counterData.count}}</span>
-      <button @click="increaseCounter" class="btn px-5 bg-gray-100">+</button>
+      <button @click="increaseCounter(1, $event)" class="btn px-5 bg-gray-100">+1</button>
+      <button @click="increaseCounter(2)" class="btn px-5 bg-gray-100">+2</button>
     </div>
+    <p>This counter is {{oddOrEven}}.</p>
+
     <div class="edit mt-5">
       <h4 class="m-1">Edit Counter Title:</h4>
       <input v-model="counterData.title" class="bg-gray-200 border border-black text-darkgray rounded p-1" type="text" >
@@ -14,19 +19,33 @@
   </div>
 </template>
 
-<script setup>
-  import { reactive, ref } from 'vue'
-  const counterData = reactive({
-    count: 0,
-    title: 'Super Counter'
-  });
 
-  const increaseCounter = () => {
-    counterData.count++;
-  }
-  const decreaseCounter = () => {
-    counterData.count--;
-  }
+
+<script setup> // src/views/HomeView.vue
+
+import { computed, reactive, ref } from 'vue'
+
+const appTitle = "My Amazing Counter App";
+
+const counterData = reactive({
+  count: 0,
+  title: 'Super Counter'
+});
+
+const oddOrEven = computed(() => {
+  if(counterData.count % 2 === 0) return 'even';
+  return 'odd';
+});
+
+
+const increaseCounter = (amount, event) => {
+  // console.log(event);
+  counterData.count += amount;
+}
+const decreaseCounter = amount => {
+  counterData.count -= amount;;
+}
+
 </script>
 
 
